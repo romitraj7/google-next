115cb2cb3dddaa11129034cdc2771cb1626273ed
Romit <romit2884@gmail.com>
2024-04-06 23:54:07 +0530

    add Home search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <HomeHeader/>
+    <>
+      <HomeHeader />
+      <div className='flex flex-col items-center mt-24'>
+        <Image alt="Google Logo" src={'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png'} width={300} height={100}/>
+        <HomeSearch/>
+      </div>
+    </>
   );
 }
