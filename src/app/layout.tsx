cca85119668c27f93324dc9630cc9232c4a292d2
Romit <romit2884@gmail.com>
2024-04-08 23:02:33 +0530

    home layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang='en'>
+      <body className='relative min-h-screen'>
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
